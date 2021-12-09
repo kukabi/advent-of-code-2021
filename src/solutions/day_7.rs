617eff1714d10774ff6cd4d8aef0f0a62b1bd206
kukabi <kutsalbilgin@gmail.com>
2021-12-09 22:53:06 +0300

    Day 7-b solution.

--- src/solutions/day_7.rs
+++ src/solutions/day_7.rs
@@ -11,13 +11,29 @@ pub fn day_7_a() {
         .map(|num| num.parse().unwrap())
         .sorted()
         .collect();
-    let position = nums[nums.len() / 2];
-    println!(
-        "7a: {}",
-        nums.iter().map(|a| (a - position).abs()).sum::<i32>()
-    );
+    let median = nums[nums.len() / 2];
+    let result = nums.iter().map(|x| (x - median).abs()).sum::<i32>();
+    println!("7a: {}", result);
 }
 
 pub fn day_7_b() {
-    todo!("7-a is not ready.")
+    fn consumption(x: i32) -> i32 {
+        if x == 0 {
+            0
+        } else {
+            x + consumption(x - 1)
+        }
+    }
+    let nums: Vec<i32> = include_str!("../../input/day_7.txt")
+        .split(',')
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let avg_floor = (nums.iter().sum::<i32>() as f32 / nums.len() as f32).floor() as i32;
+    println!(
+        "7b: {}",
+        (avg_floor..(avg_floor + 2))
+            .map(|a| nums.iter().map(|x| consumption((x - a).abs())).sum::<i32>())
+            .min()
+            .unwrap()
+    );
 }
